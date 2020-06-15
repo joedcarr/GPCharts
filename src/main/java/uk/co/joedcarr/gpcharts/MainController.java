ee361e7390c64c3a3bd2e8fbccb8f2e6016585c4
joedcarr <joedcarr@gmail.com>
2020-06-15 11:40:42 +0100

    Load results for a single race.

--- src/main/java/uk/co/joedcarr/gpcharts/MainController.java
+++ src/main/java/uk/co/joedcarr/gpcharts/MainController.java
@@ -1,7 +1,6 @@
 package uk.co.joedcarr.gpcharts;
 
 import javafx.beans.property.ReadOnlyStringWrapper;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -9,8 +8,6 @@ import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.util.Callback;
 import ru.olerom.formula.ergast.Ergast;
 import ru.olerom.formula.ergast.objects.Driver;
 import ru.olerom.formula.ergast.objects.RaceResult;
@@ -31,24 +28,12 @@ public class MainController {
     private final int MIN_YEAR = 1950;
     private final int MAX_YEAR = Year.now().minusYears(1).getValue();
     @FXML
-    //private TableView<Driver> resultsTable;
     private TableView<Result> resultsTable;
 
     @FXML
     private void initialize() {
         seasonSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(MIN_YEAR, MAX_YEAR));
     }
-
-    //@FXML
-    /*private void loadResults() throws IOException {
-        api = new Ergast(seasonSpinner.getValue(), Ergast.DEFAULT_LIMIT, Ergast.DEFAULT_OFFSET);
-        resultsTable.getItems().clear();
-        resultsTable.getColumns().clear();
-        var driverCol = new TableColumn<Driver, String>("Driver");
-        driverCol.setCellValueFactory(new PropertyValueFactory<>("familyName"));
-        resultsTable.getColumns().add(driverCol);
-        resultsTable.getItems().addAll(api.getDrivers());
-    }*/
 
     @FXML
     private void loadResults() throws IOException {
@@ -58,13 +43,16 @@ public class MainController {
 
         ObservableList<Result> results = FXCollections.observableArrayList();
         List<Driver> drivers = api.getDrivers();
-        drivers.forEach(driver -> results.add(new Result(driver)));
+        // Move to Map for race result in result model.
+        List<RaceResult> raceResults = new ArrayList<>(api.getSchedules().size());
+        drivers.forEach(driver -> results.add(new Result(driver, raceResults)));
         Schedule schedule = api.getSchedules().get(0);
-        List<RaceResult> raceResults = api.getRaceResults(schedule.getRound());
+        List<RaceResult> roundResults = api.getRaceResults(schedule.getRound());
 
-        for(RaceResult raceResult : raceResults) {
-            Optional<Result> result = results.stream().filter(r -> r.getDriver() == raceResult.getDriver()).findFirst();
-            result.ifPresent(value -> value.addRaceResult(schedule.getRound(), raceResult));
+        for(RaceResult roundResult : roundResults) {
+            Optional<Result> result = results.stream().filter(r -> r.getDriver().getDriverId().equals(roundResult
+                    .getDriver().getDriverId())).findFirst();
+            result.ifPresent(value -> value.addRaceResult(schedule, roundResult));
         }
 
         var driverCol = new TableColumn<Result, String>("Driver");
@@ -72,7 +60,11 @@ public class MainController {
             Driver driver = features.getValue().getDriver();
             return new ReadOnlyStringWrapper(driver.getGivenName().charAt(0) + " " + driver.getFamilyName());
         });
+        var raceCol = new TableColumn<Result, String>(schedule.getRaceName().substring(0, 3).toUpperCase());
+        raceCol.setCellValueFactory(features -> new ReadOnlyStringWrapper(features.getValue().getRaceResults()
+                .get(schedule).getPositionText()));
         resultsTable.getColumns().add(driverCol);
+        resultsTable.getColumns().add(raceCol);
         resultsTable.getItems().addAll(results);
     }
 
--- src/main/java/uk/co/joedcarr/gpcharts/models/Result.java
+++ src/main/java/uk/co/joedcarr/gpcharts/models/Result.java
@@ -1,32 +1,38 @@
 package uk.co.joedcarr.gpcharts.models;
 
 import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleMapProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 import ru.olerom.formula.ergast.objects.Driver;
 import ru.olerom.formula.ergast.objects.RaceResult;
+import ru.olerom.formula.ergast.objects.Schedule;
 
 import java.util.List;
+import java.util.Map;
 
 public class Result {
     private final SimpleObjectProperty<Driver> driver;
-    private final SimpleListProperty<RaceResult> raceResults;
+    //private final SimpleListProperty<RaceResult> raceResults;
+    private final SimpleMapProperty<Schedule, RaceResult> raceResults;
 
-    public Result(Driver driver) {
+    public Result(Driver driver, List<RaceResult> raceResults) {
         this.driver = new SimpleObjectProperty<>(driver);
-        this.raceResults = new SimpleListProperty<>(FXCollections.emptyObservableList());
+        //this.raceResults = new SimpleListProperty<>(FXCollections.observableArrayList(raceResults));
+        this.raceResults = new SimpleMapProperty<>(FXCollections.observableHashMap());
     }
 
-    public void addRaceResult(int round, RaceResult raceResult) {
-        raceResults.add(round, raceResult);
+    public void addRaceResult(Schedule round, RaceResult raceResult) {
+        raceResults.put(round, raceResult);
     }
 
     public Driver getDriver() {
         return driver.get();
     }
 
-    public ObservableList<RaceResult> getRaceResults() {
+    public Map<Schedule, RaceResult> getRaceResults() {
         return raceResults.get();
     }
 
@@ -34,7 +40,7 @@ public class Result {
         return driver;
     }
 
-    public SimpleListProperty<RaceResult> raceResultsProperty() {
+    public SimpleMapProperty<Schedule, RaceResult> raceResultsProperty() {
         return raceResults;
     }
 
